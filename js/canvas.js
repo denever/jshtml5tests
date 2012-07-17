54be3c50b5d723068907c6cd7c3f1652005689c6
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-07-17 09:05:51 +0200

    Spostato js in file seprato

---
+++ js/canvas.js
@@ -0,0 +1,7 @@
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+ctx.font="30px Arial";
+ctx.fillText("News", 50, 100);
+ctx.fillText("Works", 300, 120);
+ctx.fillText("Contacts", 30, 350);
+ctx.fillText("About", 400, 250);
