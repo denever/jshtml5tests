36072c27245afd87b36ffedefc2f3892ff0a4fc2
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-07-30 09:00:55 +0200

    Corretti nomi identificatori

--- js/mysliders.js
+++ js/mysliders.js
@@ -39,7 +39,7 @@ function onload(){
 	    max: 1,
 	    step: 0.1,
 	    slide: function( event, ui ) {
-		$( "#about" ).fadeTo('fast', ui.value);
+		$( "#news" ).fadeTo('fast', ui.value);
 		}
 	    }
     );
@@ -50,7 +50,7 @@ function onload(){
 	    max: 1,
 	    step: 0.1,
 	    slide: function( event, ui ) {
-		$( "#about" ).fadeTo('fast', ui.value);
+		$( "#fashion" ).fadeTo('fast', ui.value);
 		}
 	    }
     );
@@ -61,7 +61,7 @@ function onload(){
 	    max: 1,
 	    step: 0.1,
 	    slide: function( event, ui ) {
-		$( "#about" ).fadeTo('fast', ui.value);
+		$( "#special" ).fadeTo('fast', ui.value);
 		}
 	    }
     );
